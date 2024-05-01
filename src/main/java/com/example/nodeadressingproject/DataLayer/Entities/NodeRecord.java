1ea1226b398caec87195b7bc8fb7cd91abef85f8
Ali Berkay Görgülü <aliberkay.gorgulu@hotmail.com>
2024-05-02 01:32:37 +0300

    public-key field added

--- src/main/java/com/example/nodeadressingproject/DataLayer/Entities/NodeRecord.java
+++ src/main/java/com/example/nodeadressingproject/DataLayer/Entities/NodeRecord.java
@@ -1,16 +1,13 @@
 package com.example.nodeadressingproject.DataLayer.Entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.UUID;
 
 /**
  * Entity class representing a node record in the blockchain.
- * Each node record includes a unique identifier (UUID), IP address, and an activation status.
+ * Each node record includes a unique identifier (UUID), IP address, an activation status, and a public key in byte array format.
  */
 @Entity
 @Data
@@ -27,17 +24,23 @@ public class NodeRecord {
     @Column(name = "is-active")
     boolean isActive;
 
+    @Lob
+    @Column(name = "public-key", columnDefinition="BLOB")
+    private byte[] publicKey;
+
     /**
-     * Constructor for creating a NodeRecord with a specified UUID, IP address, and activation status.
+     * Constructor for creating a NodeRecord with a specified UUID, IP address, activation status, and a public key.
      *
      * @param uuid      The unique identifier (UUID) of the node.
-     * @param ipAddress  The IP address of the node.
+     * @param ipAddress The IP address of the node.
      * @param isActive  The activation status of the node.
+     * @param publicKey The public key of the node.
      */
-    public NodeRecord(UUID uuid, String ipAddress, boolean isActive){
+    public NodeRecord(UUID uuid, String ipAddress, boolean isActive, byte[] publicKey){
         this.ipAddress = ipAddress;
         this.uuid = uuid;
         this.isActive = isActive;
+        this.publicKey = publicKey;
     }
 
     /**
@@ -45,8 +48,9 @@ public class NodeRecord {
      */
     public NodeRecord(){
         this.uuid = null;
-        this.ipAddress =null;
+        this.ipAddress = null;
         this.isActive = false;
+        this.publicKey = null;
     }
 }
 
